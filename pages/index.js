cbcef5ca1f1d3a7500a351ae2ec6844fb422bfd3
GST <g.samtor@gmail.com>
2021-05-11 23:11:32 -0300

    index changes

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Gustavo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <navbar />
       <main>
-        <h1 className="title">G u s t a v o T o r r e s</h1>
+        <h1 className="title">GustavoTorres</h1>
 
         <p className="description">
           <code>Compreender fenômenos, sistematizar problemas e planejar soluções.</code>
